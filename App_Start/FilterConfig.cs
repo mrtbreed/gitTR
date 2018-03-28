136de339c9c6e3e0550288129a5c11ecc97ef5b7
mrtbreed <36203472+mrtbreed@users.noreply.github.com>
2018-03-28 09:18:40 -0700

    AboutMeMVC
    You Will have to add NuGet packages and a SendGrid to make the app function correctly;  you can remove authorize and just run it that way as well.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace About_TR
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace About_TR.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "N/A";
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Info";
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Bio()
+        {
+            ViewBag.Message = "Bio";
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Movies()
+        {
+            ViewBag.Message = "Movies";
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Books()
+        {
+            ViewBag.Message = "Books";
+
+            return View();
+        }
+    }
+}
